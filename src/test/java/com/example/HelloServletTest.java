88d36de0fa958da5a80d6d605f175319941abb07
sneha-nayak546 <nayaksneha628@gmail.com>
2026-04-08 19:37:53 +0530

    Update HelloServletTest.java

--- src/test/java/com/example/HelloServletTest.java
+++ src/test/java/com/example/HelloServletTest.java
@@ -1,19 +1,21 @@
 package com.example;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.junit.jupiter.api.Test;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import org.junit.jupiter.api.Test;
 
 public class HelloServletTest {
 
     @Test
-    void testDoGet() throws Exception {
+    public void testDoGet() throws Exception {
         HelloServlet servlet = new HelloServlet();
 
         HttpServletRequest request = mock(HttpServletRequest.class);
@@ -27,14 +29,8 @@ public class HelloServletTest {
         servlet.doGet(request, response);
 
         writer.flush();
-        String result = stringWriter.toString();
-
-        assert(result.contains("Deployment Successful!"));
-        assert(result.contains("GitHub"));
-        assert(result.contains("Jenkins"));
-        assert(result.contains("Docker"));
-        assert(result.contains("Tomcat 11"));
 
         verify(response).setContentType("text/html");
+        assert stringWriter.toString().contains("Hello from Servlet");
     }
 }
